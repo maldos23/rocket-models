eb6ad784c9eb65e8e3cb00c64d7d03b2fbd07173
maldos23 <yosiquesealgo@gmail.com>
2024-06-17 06:03:34 -0600

    fix: Change impor in unit test

--- test/db.test.js
+++ test/db.test.js
@@ -1,11 +1,11 @@
 import { expect } from "chai";
 import sinon from "sinon";
-import { rocket } from "../index.js";
+import { db } from "../index.js";
 
 describe("initDatabase", () => {
   it("should throw an error if no URI is provided", async () => {
     try {
-      await rocket.db.initDatabase();
+      await db.initDatabase();
     } catch (err) {
       expect(err.message).to.equal("URI is required");
     }
@@ -13,7 +13,7 @@ describe("initDatabase", () => {
 
   it("should throw an error if an invalid variant is provided", async () => {
     try {
-      await rocket.db.initDatabase("mongodb://localhost:27017/myapp", {
+      await db.initDatabase("mongodb://localhost:27017/myapp", {
         variant: "invalid",
       });
     } catch (err) {
@@ -23,9 +23,9 @@ describe("initDatabase", () => {
 
   it("should call initMongo if variant is mongo", async () => {
     const initMongoStub = sinon
-      .stub(rocket.db, "initDatabase")
+      .stub(db, "initDatabase")
       .returns(Promise.resolve());
-    await rocket.db.initDatabase("mongodb://localhost:27017/myapp", {
+    await db.initDatabase("mongodb://localhost:27017/myapp", {
       variant: "mongo",
     });
     expect(initMongoStub.calledOnce).to.be.true;
@@ -34,9 +34,9 @@ describe("initDatabase", () => {
 
   it("should call initPostgress if variant is postgres", async () => {
     const initPostgressStub = sinon
-      .stub(rocket.db, "initDatabase")
+      .stub(db, "initDatabase")
       .returns(Promise.resolve());
-    await rocket.db.initDatabase("postgresql://localhost:5432/myapp", {
+    await db.initDatabase("postgresql://localhost:5432/myapp", {
       variant: "postgres",
     });
     expect(initPostgressStub.calledOnce).to.be.true;
